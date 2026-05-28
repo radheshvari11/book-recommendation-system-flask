4bc996de998ff7f2cd168657437262e3b2d89e1f
radheshvari11 <sakshineman192@gmail.com>
2026-05-28 09:10:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,35 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
+# Load dataset
 df = pd.read_csv("data/books.csv")
+
+# Create similarity matrix
+cv = CountVectorizer()
+count_matrix = cv.fit_transform(df['Book'])
+
+similarity = cosine_similarity(count_matrix)
 
 def recommend(book_name):
 
-    recommendations = df[df['Book'] != book_name]['Book'].head(3).tolist()
+    recommendations = []
+
+    try:
+        index = df[df['Book'] == book_name].index[0]
+
+        scores = list(enumerate(similarity[index]))
+
+        sorted_scores = sorted(
+            scores,
+            key=lambda x: x[1],
+            reverse=True
+        )
+
+        for i in sorted_scores[1:4]:
+            recommendations.append(df.iloc[i[0]]['Book'])
+
+    except:
+        recommendations = ["No recommendations found"]
 
     return recommendations
